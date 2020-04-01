e706d131dd63dab6de783a73e3707c42c1e433b5
Yulia Karpova <karpova.jul@gmail.com>
2020-04-01 19:30:05 +1100

    Добавляет функцию рендеринга компонентов и выводит их на странице

--- src/main.js
+++ src/main.js
@@ -322,3 +322,32 @@ const createTripEventItemTemplate = () => {
     </li>`
   );
 };
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripMainElement = document.querySelector(`.trip-main`);
+const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
+const tripControlsFilterHeaderElement = tripControlsElement.querySelector(`h2:nth-of-type(2)`);
+
+render(tripControlsFilterHeaderElement, createSiteMenuTemplate(), `beforebegin`);
+render(tripControlsElement, createSiteFilterTemplate(), `beforeend`);
+render(tripControlsElement, createTripMainInfoSectionElement(), `beforebegin`);
+
+const tripMainInfoSectionElement = tripMainElement.querySelector(`.trip-info`);
+render(tripMainInfoSectionElement, createTripMainInfoElement(), `beforeend`);
+render(tripMainInfoSectionElement, createTripInfoCostElement(), `beforeend`);
+
+const tripEventsElement = document.querySelector(`.trip-events`);
+const tripEventsHeaderElement = tripEventsElement.querySelector(`h2`);
+
+render(tripEventsHeaderElement, createTripSortTemplate(), `afterend`);
+render(tripEventsElement, createTripDaysTemplate(), `beforeend`);
+
+const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
+render(tripEventsListElement, createTripEventEditFormTemplate(), `beforeend`);
+
+for (let i = 0; i < EVENTS_COUNT; i++) {
+  render(tripEventsListElement, createTripEventItemTemplate(), `beforeend`);
+}
