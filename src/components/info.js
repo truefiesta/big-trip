36687d2ea67e0507e96e886244412adcdd044acf
Yulia Karpova <karpova.jul@gmail.com>
2020-04-04 12:04:33 +1100

    Разбивает main.js на модули и подключает их.

---
+++ src/components/info.js
@@ -0,0 +1,16 @@
+const createTripMainInfoSectionElement = () => {
+  return `<section class="trip-main__trip-info  trip-info"></section>`;
+};
+
+// Информация о маршруте (доп.задание);
+const createTripMainInfoElement = () => {
+  return (
+    `<div class="trip-info__main">
+      <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+    </div>`
+  );
+};
+
+export {createTripMainInfoSectionElement, createTripMainInfoElement};
