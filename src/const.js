b602e919790e5ec68a25dd09b0da5bbf26b74595
Yulia Karpova <karpova.jul@gmail.com>
2020-04-14 00:17:46 +1000

    Добавляет файл с константами.

---
+++ src/const.js
@@ -0,0 +1,50 @@
+export const eventTypes = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`,
+  `Check`,
+  `Sightseeing`,
+  `Restaurant`
+];
+
+export const destinations = [
+  `Amsterdam`,
+  `Geneva`,
+  `Chamonix`,
+  `Saint Petersburg`
+];
+
+export const transferTypes = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`
+];
+
+export const activityTypes = [
+  `Check`,
+  `Sightseeing`,
+  `Restaurant`
+];
+
+export const MONTH_NAMES = [
+  `JAN`,
+  `FEB`,
+  `MAR`,
+  `APR`,
+  `MAY`,
+  `JUN`,
+  `JUL`,
+  `AUG`,
+  `SEP`,
+  `OCT`,
+  `NOV`,
+  `DEC`,
+];
