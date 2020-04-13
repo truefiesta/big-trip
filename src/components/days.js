3d923ca1d77a355207296e615b2234026ebc1ec3
Yulia Karpova <karpova.jul@gmail.com>
2020-04-14 00:27:52 +1000

    Разбивает функцию для вывода разметки дней на более мелкие и добавляет вывод данных из объекта-события.

--- src/components/days.js
+++ src/components/days.js
@@ -1,13 +1,38 @@
+import {createTripEventItemTemplate} from "../components/event.js";
+import {MONTH_NAMES} from "../const.js";
+import {castTimeFormat} from "../utils.js";
+
+const dayInfoElementsTemplate = (count, dateString) => {
+  const dateObj = JSON.parse(dateString);
+  const {date, month, year} = dateObj;
+  const newDateString = `${year}-${month}-${date}`;
+  return (
+    `<span class="day__counter">${count}</span>
+    <time class="day__date" datetime="${newDateString}">${MONTH_NAMES[month]} ${castTimeFormat(date)}</time>`
+  );
+};
+
+export const createTripDayTemplate = (dayWithEvents) => {
+  const {eventSort, daysCount, uniqDate, events} = dayWithEvents;
+  const dayInfoMarkup = eventSort ? dayInfoElementsTemplate(daysCount, uniqDate) : ``;
+  const eventsMarkup = events.map((event) => {
+    return createTripEventItemTemplate(event);
+  }).join(`\n`);
+
+  return (
+    `<li class="trip-days__item  day">
+      <div class="day__info">
+        ${dayInfoMarkup}
+      </div>
+      <ul class="trip-events__list">
+      ${eventsMarkup}
+      </ul>
+    </li>`
+  );
+};
+
 export const createTripDaysTemplate = () => {
   return (
-    `<ul class="trip-days">
-      <li class="trip-days__item  day">
-        <div class="day__info">
-          <span class="day__counter">1</span>
-          <time class="day__date" datetime="2019-03-18">MAR 18</time>
-        </div>
-        <ul class="trip-events__list"></ul>
-      </li>
-    </ul>`
+    `<ul class="trip-days"></ul>`
   );
 };
