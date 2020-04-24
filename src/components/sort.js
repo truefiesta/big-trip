af74ebbe95515b0d2e1aa19cc0ed385ceb45a836
Yulia Karpova <karpova.jul@gmail.com>
2020-04-25 06:48:12 +1000

    Личный проект: разделяй и властвуй (Часть 2) (#8)
    * Меняет метод render у контроллера путешествия, перенося в нее из main.js создание и отрисовку компонентов NoEvents, Sort и Days.
    
    * Создает компоненты NoEvents, Days и Sort один раз в конструкторе класса TripController.
    
    * Меняет функцию создания шаблона сортировки.
    
    * Переносит названия методов сортировки в const.js
    
    * Revert "Переносит названия методов сортировки в const.js"
    
    This reverts commit c99da33fbb32e5e28b452246005e993797e81def.
    
    * Создает перечисление с типами сортировки.
    
    * Убирает пустую строку и переносит часть содержимого тега input на вторую строку.
    
    * Добавляет к компонент сортировки метод для подписки на изменение активного типа сортировки.
    
    * Реализует рендер событий в обработчике изменения типа сортировки.

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,36 +1,64 @@
 import AbstractComponent from "../components/abstract-component.js";
 
-const createTripSortTemplate = () => {
+const sortNames = [`Event`, `Time`, `Price`];
+// Что это делают эти классы (trip-sort__btn--active и trip-sort__btn--by-increase)?
+// <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time">
+const createSortItemTemplate = (sortName, isChecked) => {
+  return (
+    `<div class="trip-sort__item  trip-sort__item--${sortName.toLowerCase()}">
+      <input id="sort-${sortName.toLowerCase()}" class="trip-sort__input  visually-hidden" type="radio"
+        name="trip-sort" value="sort-${sortName.toLowerCase()}" ${isChecked ? `checked` : ``}>
+      <label class="trip-sort__btn" for="sort-${sortName.toLowerCase()}">${sortName}</label>
+    </div>`
+  );
+};
+
+const createTripSortTemplate = (sortTypes) => {
+  const sortItemsMarkup = sortTypes.map((sortType, i) =>
+    createSortItemTemplate(sortType, i === 0)).join(`\n`);
+
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <span class="trip-sort__item  trip-sort__item--day">Day</span>
-
-      <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-        <label class="trip-sort__btn" for="sort-event">Event</label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-        <label class="trip-sort__btn  trip-sort__btn--active  trip-sort__btn--by-increase" for="sort-time">
-          Time
-        </label>
-      </div>
-
-      <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-        <label class="trip-sort__btn" for="sort-price">
-          Price
-        </label>
-      </div>
-
+      ${sortItemsMarkup}
       <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
     </form>`
   );
 };
 
-export default class Filter extends AbstractComponent {
+export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortName = `sort-${sortNames[0].toLowerCase()}`;
+  }
+
   getTemplate() {
-    return createTripSortTemplate();
+    return createTripSortTemplate(sortNames);
+  }
+
+  getSortName() {
+    return this._currentSortName;
+  }
+
+  setSortNameChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName.toLowerCase() !== `label`) {
+        return;
+      }
+
+      const sortName = evt.target.getAttribute(`for`);
+
+      if (this._currentSortName === sortName) {
+        return;
+      }
+      // Как это сделать правильно?
+      this.getElement().querySelector(`input:checked`).removeAttribute(`checked`);
+      this.getElement().querySelector(`input[value=${sortName}]`).setAttribute(`checked`, `true`);
+
+      this._currentSortName = sortName;
+      handler(this._currentSortName);
+    });
   }
 }
--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -1,7 +1,10 @@
 import {render, RenderPosition, replace} from "../utils/render.js";
-import {ESCAPE_KEY, ESC_KEY} from "../const.js";
+import {ESCAPE_KEY, ESC_KEY, SortType} from "../const.js";
 import DayComponent from "../components/day.js";
 import DayInfoComponent from "../components/day-info.js";
+import NoEventsComponent from "../components/no-events.js";
+import SortComponent from "../components/sort.js";
+import DaysComponent from "../components/days.js";
 import EventsComponent from "../components/events.js";
 import EventComponent from "../components/event.js";
 import EventEditComponent from "../components/event-edit.js";
@@ -48,6 +51,24 @@ const groupEventsByStartDate = (dateString, eventsArray) => {
   });
 };
 
+const getSortedEvents = (events, sortType) => {
+  let sortedEvents = [];
+  const allEvents = events.slice();
+
+  switch (sortType) {
+    case SortType.SORT_EVENT: sortedEvents = allEvents.sort((a, b) =>
+      a.time.startTime - b.time.startTime);
+      break;
+    case SortType.SORT_TIME: sortedEvents = allEvents.sort((a, b) =>
+      (a.time.endTime - a.time.startTime) - (b.time.endTime - b.time.startTime));
+      break;
+    case SortType.SORT_PRICE: sortedEvents = allEvents.sort((a, b) =>
+      b.price - a.price);
+      break;
+  }
+
+  return sortedEvents;
+};
 // Для каждого дня (уникальной начальной даты) нужно сформировать разметку с выводом
 // шаблона для дня и шаблона событий, соответствующих дню.
 // Если сортировка не по event, то числа нужно спрятать. Все события будут выводиться в один
@@ -55,23 +76,18 @@ const groupEventsByStartDate = (dateString, eventsArray) => {
 
 // Events выводятся в days
 // Сформируем структуру данных, в которой будут события по дате, фильтр, начальные даты.
-const prepareDaysWithEventsBeforeRendering = (events, sort) => {
-  sort = document.querySelector(`input[name=trip-sort]:checked`);
+const prepareDaysWithEventsBeforeRendering = (events, sortType) => {
   const daysAndEvents = [];
   let daysCount = 0;
 
-  const sortedByStartDayEvents = events.slice().sort(
-      function (a, b) {
-        return a.time.startTime - b.time.startTime;
-      }
-  );
+  const sortedEvents = getSortedEvents(events, sortType);
 
-  if (sort.value === `sort-event`) {
-    const startDates = getEventsStartDates(sortedByStartDayEvents);
+  if (sortType === SortType.SORT_EVENT) {
+    const startDates = getEventsStartDates(sortedEvents);
     const uniqDates = getEventsUniqueDates(startDates);
 
     for (const uniqDate of uniqDates) {
-      const eventsByDate = groupEventsByStartDate(uniqDate, sortedByStartDayEvents);
+      const eventsByDate = groupEventsByStartDate(uniqDate, sortedEvents);
       daysCount++;
 
       daysAndEvents.push({
@@ -86,7 +102,7 @@ const prepareDaysWithEventsBeforeRendering = (events, sort) => {
       eventSort: false,
       daysCount: 1,
       uniqDate: ``,
-      events: sortedByStartDayEvents
+      events: sortedEvents
     });
   }
 
@@ -146,8 +162,8 @@ const renderEvents = (dayComponent, events) => {
 //   - в DayComponent отрисовать DayInfoComponent (<div class="day__info"> (count, dateString) </div>)
 //   - в DayComponent же отрисовть EventsComponent (<ul class="trip-events__list"></ul>)
 //      - в EventsComponent отрисовать EventComponent (<li class="trip-events__item"> (events) </li>)
-const renderDaysWithEvents = (tripDaysComponent, allEvents) => {
-  const daysWithEvents = prepareDaysWithEventsBeforeRendering(allEvents);
+const renderDaysWithEvents = (tripDaysComponent, allEvents, sortType) => {
+  const daysWithEvents = prepareDaysWithEventsBeforeRendering(allEvents, sortType);
 
   daysWithEvents.forEach((dayWithEvents) => {
     const {eventSort, daysCount, uniqDate, events} = dayWithEvents;
@@ -161,13 +177,30 @@ const renderDaysWithEvents = (tripDaysComponent, allEvents) => {
   });
 };
 
-
 export default class TripController {
   constructor(container) {
     this._container = container;
+    this._noEventsComponent = new NoEventsComponent();
+    this._sortComponent = new SortComponent();
+    this._daysComponent = new DaysComponent();
   }
 
   render(events) {
-    renderDaysWithEvents(this._container, events);
+    const tripEventsHeaderElement = this._container.querySelector(`h2`);
+
+    if (events.length <= 0) {
+      render(tripEventsHeaderElement, this._noEventsComponent, RenderPosition.AFTER);
+      return;
+    }
+
+    render(tripEventsHeaderElement, this._sortComponent, RenderPosition.AFTER);
+    render(this._container, this._daysComponent, RenderPosition.BEFOREEND);
+
+    renderDaysWithEvents(this._daysComponent, events, SortType.SORT_EVENT);
+
+    this._sortComponent.setSortNameChangeHandler((sortType) => {
+      this._daysComponent.getElement().innerHTML = ``;
+      renderDaysWithEvents(this._daysComponent, events, sortType);
+    });
   }
 }
