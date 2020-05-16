ba1167d8f72ea7599779d43fe4155ee27aa97e59
Yulia Karpova <karpova.jul@gmail.com>
2020-05-16 19:52:24 +1000

    Выносит разметку формы Favorite и кнопки закрытия формы в отдельную функцию, так как они не должны отображаться при создании события.

--- src/components/event-edit.js
+++ src/components/event-edit.js
@@ -100,6 +100,22 @@ const createDestinationInfoMarkup = (destinationInformation) => {
   );
 };
 
+const createFavoriteButtonTemplate = (isFavorite) => {
+  const favoriteButton = isFavorite ? `checked` : ``;
+  return (
+    `<input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${favoriteButton}>
+    <label class="event__favorite-btn" for="event-favorite-1">
+      <span class="visually-hidden">Add to favorite</span>
+      <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+        <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+      </svg>
+    </label>
+    <button class="event__rollup-btn" type="button">
+    <span class="visually-hidden">Open event</span>
+    </button>`
+  );
+};
+
 const createDestinationOptionsMarkup = () => {
   return destinations.map((destination) => {
     return (
@@ -115,9 +131,8 @@ const checkType = (type) => {
   return types.includes(type) ? ` in` : ` to`;
 };
 
-const createTripEventEditFormTemplate = (event, options = {}) => {
-  const {time, price, isFavorite} = event;
-  const {type, destination, offers, destinationInfo} = options;
+const createTripEventEditFormTemplate = (options = {}, mode) => {
+  const {type, destination, offers, destinationInfo, price, time, isFavorite} = options;
 
   const transferTypeEventsMarkup = createEventTypesMarkup(transferTypes, type);
   const activityTypeEventsMarkup = createEventTypesMarkup(activityTypes, type);
@@ -127,7 +142,6 @@ const createTripEventEditFormTemplate = (event, options = {}) => {
   const availableOffersForEventType = offersByType[type.toLowerCase()];
   const offersSectionMarkup = availableOffersForEventType.length > 0 ? createOffersSectionMarkup(availableOffersForEventType, offers) : ``;
   const destinationInfoSectionMarkup = !destinationInfo ? `` : createDestinationInfoMarkup(destinationInfo);
-  const favoriteButton = isFavorite ? `checked` : ``;
   const destinationOptions = createDestinationOptionsMarkup();
 
   let isDescription = false;
@@ -143,22 +157,9 @@ const createTripEventEditFormTemplate = (event, options = {}) => {
   const destinationInfoSectionMarkup = (isDescription || isPhotos) ? createDestinationInfoMarkup(destinationInfo) : ``;
   const destinationOptions = createDestinationOptionsMarkup();
 
-            <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination}" list="destination-list-1">
-            <datalist id="destination-list-1">
-              ${destinationOptions}
-            </datalist>
-          </div>
+  const resetButtonName = mode === Mode.ADDING ? `Cancel` : `Delete`;
+  const favoriteAndRollupButtonsMarkup = mode === Mode.ADDING ? `` : createFavoriteButtonTemplate(isFavorite);
 
-          <div class="event__field-group  event__field-group--time">
-            <label class="visually-hidden" for="event-start-time-1">
-              From
-            </label>
-            <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startTime}">
-            &mdash;
-            <label class="visually-hidden" for="event-end-time-1">
-              To
-            </label>
-            <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endTime}">
   return (
     `<form class="event trip-events__item event--edit" action="#" method="post">
       <header class="event__header">
@@ -182,39 +183,46 @@ const createTripEventEditFormTemplate = (event, options = {}) => {
           </div>
         </div>
 
-          <div class="event__field-group  event__field-group--price">
-            <label class="event__label" for="event-price-1">
-              <span class="visually-hidden">Price</span>
-              &euro;
-            </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
-          </div>
         <div class="event__field-group  event__field-group--destination">
           <label class="event__label  event__type-output" for="event-destination-1">
             ${capitalize(type)} ${isActionType}
           </label>
+          <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destination}" list="destination-list-1">
+          <datalist id="destination-list-1">
+            ${destinationOptions}
+          </datalist>
+        </div>
 
-          <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-          <button class="event__reset-btn" type="reset">Delete</button>
-
-          <input id="event-favorite-1" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${favoriteButton}>
-          <label class="event__favorite-btn" for="event-favorite-1">
-            <span class="visually-hidden">Add to favorite</span>
-            <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-              <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-            </svg>
+        <div class="event__field-group  event__field-group--time">
+          <label class="visually-hidden" for="event-start-time-1">
+            From
           </label>
+          <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startTime}">
+          &mdash;
+          <label class="visually-hidden" for="event-end-time-1">
+            To
+          </label>
+          <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endTime}">
+        </div>
 
-          <button class="event__rollup-btn" type="button">
-            <span class="visually-hidden">Open event</span>
-          </button>
-        </header>
-        <section class="event__details">
-          ${offersSectionMarkup}
-          ${destinationInfoSectionMarkup}
-        </section>
-      </form>
-    </li>`
+        <div class="event__field-group  event__field-group--price">
+          <label class="event__label" for="event-price-1">
+            <span class="visually-hidden">Price</span>
+            &euro;
+          </label>
+          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${price}">
+        </div>
+
+        <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+        <button class="event__reset-btn" type="reset">${resetButtonName}</button>
+
+        ${favoriteAndRollupButtonsMarkup}
+      </header>
+      <section class="event__details">
+        ${offersSectionMarkup}
+        ${destinationInfoSectionMarkup}
+      </section>
+    </form>`
   );
 };
 
@@ -283,6 +291,7 @@ export default class EventEdit extends AbstractSmartComponent {
     this._event = event;
     this._mode = mode;
     this._copyEventFields(event);
+    this._isFavorite = event.isFavorite;
     this._submitHandler = null;
     this._favoriteHandler = null;
     this._closeHandler = null;
@@ -297,15 +306,24 @@ export default class EventEdit extends AbstractSmartComponent {
   }
 
   setEventEditFormSubmitHandler(handler) {
-    this.getElement().querySelector(`form`).addEventListener(`submit`, handler);
+    this.getElement().addEventListener(`submit`, (evt) => {
+      evt.preventDefault();
+      handler();
+    });
 
     this._submitHandler = handler;
   }
 
   setEventFavoriteClickHandler(handler) {
-    this.getElement().querySelector(`input[name=event-favorite]`).addEventListener(`click`, handler);
+    const favoriteButtonElement = this.getElement().querySelector(`input[name=event-favorite]`);
 
-    this._favoriteHandler = handler;
+    if (favoriteButtonElement) {
+      favoriteButtonElement.addEventListener(`click`, () => {
+        this._isFavorite = !this._isFavorite;
+        handler();
+      });
+      this._favoriteHandler = handler;
+    }
   }
 
   setDeleteButtonClickHandler(handler) {
@@ -315,9 +333,13 @@ export default class EventEdit extends AbstractSmartComponent {
   }
 
   setCloseButtonClickHandler(handler) {
-    this.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, handler);
+    const closeButtonElement = this.getElement().querySelector(`.event__rollup-btn`);
 
-    this._closeHandler = handler;
+    if (closeButtonElement) {
+      closeButtonElement.addEventListener(`click`, handler);
+
+      this._closeHandler = handler;
+    }
   }
 
   removeElement() {
