123f1fbd575c1a8ec3a7dad223feefcf4851e226
Yulia Karpova <karpova.jul@gmail.com>
2020-05-16 19:34:12 +1000

    Создает новую структуру, у которой каждому пункту назначения соответствуют фотографии и описания этого пункта назначения.

--- src/const.js
+++ src/const.js
@@ -256,6 +256,54 @@ export const EventType = {
 };
 
 export const EVENT_TYPES = Object.values(EventType);
+
+export const Destinations = [
+  {
+    "description": `Chamonix, is a beautiful city, a true asian pearl, with crowded streets.`,
+    "name": `Chamonix`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/300/200?r=0.0762563005163317`,
+        "description": `Chamonix parliament building`
+      },
+      {
+        "src": `http://picsum.photos/300/200?r=0.6737960490195023`,
+        "description": `Chamonix building`
+      }
+    ]
+  },
+  {
+    "description": `Saint-Petersburg, aliquam id orci ut lectus varius viverra. Sed sed nisi sed augue convallis suscipit in sed felis.`,
+    "name": `Saint-Petersburg`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/248/152?r=0.6737960490195023`,
+        "description": `Saint-Petersburg`
+      }
+    ]
+  },
+  {
+    "description": `Amsterdam, hasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    "name": `Amsterdam`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/248/152?r=0.24264711939060257`,
+        "description": `Amsterdam`
+      }
+    ]
+  },
+  {
+    "description": `Geneva, liquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`,
+    "name": `Geneva`,
+    "pictures": [
+      {
+        "src": `http://picsum.photos/248/152?r=0.5278029974867562`,
+        "description": `Geneva`
+      }
+    ]
+  }
+];
+
 export const Mode = {
   ADDING: `adding`,
   DEFAULT: `default`,
