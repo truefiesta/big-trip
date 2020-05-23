2f5218ace642b64bf04c8c4d388d0243b1cb9c55
Yulia Karpova <karpova.jul@gmail.com>
2020-05-23 16:36:30 +1000

    Создает модуль для получения данных с сервера.

---
+++ src/api.js
@@ -0,0 +1,32 @@
+import PointModel from "./models/point.js";
+
+export default class API {
+  constructor(authorization) {
+    this._authorization = authorization;
+  }
+
+  getEvents() {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`https://11.ecmascript.pages.academy/big-trip/points`, {headers})
+      .then((events) => events.json())
+      .then((events) => {console.log(events); return events })
+      .then(PointModel.parseEvents);
+  }
+
+  getDestinations() {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`https://11.ecmascript.pages.academy/big-trip/destinations`, {headers})
+    .then((destinations) => destinations.json());
+  }
+
+  getOffers() {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+    return fetch(`https://11.ecmascript.pages.academy/big-trip/offers`, {headers})
+    .then((offers) => offers.json());
+  }
+}
